70c45ff4845c834296850389ed754b39358e4f00
matteo113 <besancon.matteo@gmail.com>
2018-03-28 16:04:14 +0200

    Setup production jenkins server

---
+++ docker-config/jenkins/run_jenkins.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+CONT_NAME="jenkins"
+
+
+echoerr() { echo "$1" 1>&2; }
+
+if [[ "$(docker images -q jenkins-prod 2> /dev/null)" == "" ]]; then
+        docker build -t jenkins-prod .
+fi
+
+
+if [[ "$(docker ps -a -q -f "name=$CONT_NAME" 2> /dev/null)" != "" ]]; then
+        docker start $CONT_NAME
+else
+	docker run -d -p 8181:8080 -u root -v /var/run/docker.sock:/var/run/docker.sock -v jenkins-data:/var/jenkins_home --name $CONT_NAME jenkins-prod
+
+fi
