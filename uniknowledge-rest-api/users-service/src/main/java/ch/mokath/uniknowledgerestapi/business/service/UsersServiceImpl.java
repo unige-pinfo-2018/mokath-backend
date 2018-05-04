5362e1b5ab3b66e152841375b465d566b7db5539
Terry Vogelsang <terryvogelsang@gmail.com>
2018-05-04 02:59:28 +0200

    Refactor users-service

--- uniknowledge-rest-api/users-service/src/main/java/ch/mokath/uniknowledgerestapi/business/service/UsersServiceImpl.java
+++ uniknowledge-rest-api/users-service/src/main/java/ch/mokath/uniknowledgerestapi/business/service/UsersServiceImpl.java
@@ -5,7 +5,9 @@ package ch.mokath.uniknowledgerestapi.business.service;
 
 import java.security.NoSuchAlgorithmException;
 import java.security.spec.InvalidKeySpecException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 import javax.ejb.Stateless;
@@ -21,8 +23,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import ch.mokath.uniknowledgerestapi.dom.AuthInfos;
+import ch.mokath.uniknowledgerestapi.dom.Question;
 import ch.mokath.uniknowledgerestapi.dom.Token;
 import ch.mokath.uniknowledgerestapi.dom.User;
+import ch.mokath.uniknowledgerestapi.utils.DBHelper;
 
 /**
  * @author tv0g
@@ -33,7 +37,7 @@ public class UsersServiceImpl implements UsersService {
 
 	@PersistenceContext
 	private EntityManager em;
-
+	private DBHelper DBHelper = new DBHelper();
 	private Logger log = LoggerFactory.getLogger(UsersServiceImpl.class);
 
 	@Override
@@ -44,25 +48,28 @@ public class UsersServiceImpl implements UsersService {
 	@Override
 	public Optional<String> login(AuthInfos a) throws NoSuchAlgorithmException, InvalidKeySpecException {
 
-		Optional<List<User>> matchedUsers = getUsersFrom("email", a.getEmail());
-
+		Map<String, Object> wherePredicatesMapForUser = new HashMap<String, Object>();
+		wherePredicatesMapForUser.put("email", a.getEmail());
+		Optional<User> wrappedUser = DBHelper.getEntityFromFields(wherePredicatesMapForUser, User.class, em);
+		
 		// If no user matched, return an empty Optional object
-		if (matchedUsers.isPresent() == true) {
+		if (wrappedUser.isPresent()) {
 
-			User u = matchedUsers.get().get(0);
+			User u = wrappedUser.get();
 
 			if (a.validatePassword(u.getPassword())) {
 
-				// Create JWT with 24 hours expiration, return both the token and its signing
-				// key
+				// Create JWT with 24 hours expiration, return both the token and its signing key
 				List<String> JWTokenComponents = a.createJWT(u.getId(), 86400000);
 				Token JWTObject = new Token(JWTokenComponents.get(0), JWTokenComponents.get(1), u);
 
 				// Check if Token already exists for user, if yes revoke it and generate a new token
-				Optional<List<Token>> previousToken = getTokenForUser(JWTObject.getUser());
+				Map<String, Object> wherePredicatesMapForToken = new HashMap<String, Object>();
+				wherePredicatesMapForToken.put("user", JWTObject.getUser());
+				Optional<Token> wrappedToken = DBHelper.getEntityFromFields(wherePredicatesMapForToken, Token.class, em);
 
-				if (previousToken.isPresent()) {
-					revokeToken(previousToken.get().get(0));   
+				if (wrappedToken.isPresent()) {
+					revokeToken(wrappedToken.get());   
 				}
 				
 				em.persist(JWTObject);
@@ -76,48 +83,6 @@ public class UsersServiceImpl implements UsersService {
 	@Override
 	public void logout(Token t) {
 		revokeToken(t);
-	}
-
-	private <T> Optional<List<User>> getUsersFrom(String field, T value) {
-
-		// Create the Critera Builder
-		CriteriaBuilder criteriaBuilder = em.getCriteriaBuilder();
-
-		// Link Query to User Class
-		CriteriaQuery<User> criteriaQuery = criteriaBuilder.createQuery(User.class);
-		Root<User> from = criteriaQuery.from(User.class);
-
-		// Modify and create the query to match given field/value pairs entries
-		criteriaQuery.where(criteriaBuilder.equal(from.get(field), value));
-		TypedQuery<User> finalQuery = em.createQuery(criteriaQuery);
-
-		// Execute SELECT request on previous defined query predicates
-		List<User> matchedUsers = finalQuery.getResultList();
-		log.info(matchedUsers.toString());
-		// If users list is not empty, return list of users wrapped in Optional object
-		// else, return an empty Optional object
-		return matchedUsers.isEmpty() ? Optional.empty() : Optional.of(matchedUsers);
-	}
-
-	private Optional<List<Token>> getTokenForUser(User user) {
-
-		// Create the Critera Builder
-		CriteriaBuilder criteriaBuilder = em.getCriteriaBuilder();
-
-		// Link Query to Token Class
-		CriteriaQuery<Token> criteriaQuery = criteriaBuilder.createQuery(Token.class);
-		Root<Token> from = criteriaQuery.from(Token.class);
-
-		// Modify and create the query to match given field/value pairs entries
-		criteriaQuery.where(criteriaBuilder.equal(from.get("user"), user));
-		TypedQuery<Token> finalQuery = em.createQuery(criteriaQuery);
-
-		log.info("QUERY : " + finalQuery.toString());
-		// Execute SELECT request on previous defined query predicates
-		
-		List<Token> matchedTokens = finalQuery.getResultList();
-		return matchedTokens.isEmpty() ? Optional.empty() : Optional.of(matchedTokens);
-
 	}
 	
 	private void revokeToken(Token t) {
