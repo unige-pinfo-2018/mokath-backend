10da01fc60091ae6a74b3bb0fc059fe84c5b7ab7
tsq <tPXkalLi@siw.mejgalastlan.com>
2018-05-30 00:59:03 +0200

    TESTS: Base file structure/pom setup JaCoCo-Surefire

---
+++ uniknowledge-rest-api/users-service/src/test/java/ch/mokath/uniknowledgerestapi/business/service/UsersServiceImplTest.java
@@ -0,0 +1,17 @@
+package ch.mokath.uniknowledgerestapi.business.service;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+* @author ornella
+* @author zue
+*/
+public class UsersServiceImplTest {
+
+        @Test
+        public void ATest(){
+        Assert.assertTrue(true);
+        }
+
+}
---
+++ uniknowledge-rest-api/utils/src/test/java/ch/mokath/uniknowledgerestapi/utils/CustomExceptionTest.java
@@ -0,0 +1,14 @@
+package ch.mokath.uniknowledgerestapi.utils;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class CustomExceptionTest {
+
+	@Test
+	public void ATest(){
+        Assert.assertTrue(true);
+	}
+
+}
