ed93a86608479e43ad952f3a08f82ec33453c3c2
matteo113 <besancon.matteo@gmail.com>
2018-05-29 21:19:09 +0200

    Change jenkins data volume

--- docker-config/jenkins/run_jenkins.sh
+++ docker-config/jenkins/run_jenkins.sh
@@ -13,6 +13,6 @@ fi
 if [[ "$(docker ps -a -q -f "name=$CONT_NAME" 2> /dev/null)" != "" ]]; then
         docker start $CONT_NAME
 else
-	docker run -d -p 8181:8080 -u root -v /tmp/wildfly-deployments:/tmp/wildfly-deployments -v /var/run/docker.sock:/var/run/docker.sock -v jenkins-data:/var/jenkins_home --name $CONT_NAME jenkins-prod
+	docker run -d -p 8181:8080 -u root -v /tmp/wildfly-deployments:/tmp/wildfly-deployments -v /var/run/docker.sock:/var/run/docker.sock -v /home/docker-master/jenkins_home:/var/jenkins_home --name $CONT_NAME jenkins-prod
 
 fi
