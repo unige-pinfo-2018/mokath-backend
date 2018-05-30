2a976838afb78d68a8aba5e3fbd6059a160dbb0b
tsq <tPXkalLi@siw.mejgalastlan.com>
2018-05-30 08:58:54 +0200

    user: inst,tostring,builder

--- uniknowledge-rest-api/institutions-service/src/test/java/ch/mokath/uniknowledgerestapi/business/service/InstitutionsServiceImplTest.java
+++ uniknowledge-rest-api/institutions-service/src/test/java/ch/mokath/uniknowledgerestapi/business/service/InstitutionsServiceImplTest.java
@@ -3,15 +3,25 @@ package ch.mokath.uniknowledgerestapi.business.service;
 import org.junit.Assert;
 import org.junit.Test;
 
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import ch.mokath.uniknowledgerestapi.utils.DBHelper;
+
 /**
 * @author ornella
 * @author zue
 */
+@Stateless
 public class InstitutionsServiceImplTest {
+	@PersistenceContext
+	private EntityManager em;
+	private DBHelper DBHelper = new DBHelper();
 
-        @Test
-        public void ATest(){
-        Assert.assertTrue(true);
-        }
+    @Test
+    public void ATest(){
+//        Assert.assertEquals(isContactEmailOrInstitutionNameAlreadyUsed("test.email@truc.com","instName"),false);
+    }
 
 }
